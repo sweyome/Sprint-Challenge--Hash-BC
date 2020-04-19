39b1dcc67ef1265f19dc55537b07f6e2a5563f10
sweyome <sweyome1@gmail.com>
2020-04-19 19:43:46 -0400

    insert limit and list of weights

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -10,12 +10,17 @@ def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
     """
-    YOUR CODE HERE
+    Given a package with a weight limit `limit` and a list `weights` of item weights, implement a function `get_indices_of_item_weights` that finds two items whose sum of weights equals the weight limit `limit`. Your function will return an instance of an `Answer` tuple that has the following form:
+    ```
+(zero, one)
     """
-
-    return None
-
-
+    
+    for item in range(length): #! Inserts limit and lists of weights into hash table
+        hash_table_insert(ht, weights[item], item)
+     
+    
+    
+  
 def print_answer(answer):
     if answer is not None:
         print(str(answer[0] + " " + answer[1]))
